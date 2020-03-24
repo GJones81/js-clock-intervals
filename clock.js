ff33348e9c8cdfe4e322490ca9f735e529149290
gjones81 <jones.guy81@gmail.com>
2020-03-23 19:05:12 -0700

    updating

---
+++ clock.js
@@ -0,0 +1,34 @@
+let clock = document.getElementById('face')
+let second_hand = document.getElementById('second')
+let minute_hand = document.getElementById('minute')
+let hour_hand = document.getElementById('hour')
+const rotationPerSecond = 6
+const rotationPerMinute = 0.1
+const rotationPerHour = .0083
+let degreesPerSecond = 0
+let degreesPerMinute = 0
+let degreesPerHour = 0
+
+
+const secondsCounter = () => {
+	degreesPerSecond = degreesPerSecond + rotationPerSecond;
+	second_hand.style.transform = `rotate(${degreesPerSecond}deg)`
+}
+
+const minutesCounter = () => {
+	degreesPerMinute = degreesPerMinute + rotationPerMinute;
+	minute_hand.style.transform = `rotate(${degreesPerMinute}deg)`
+}
+
+const hoursCounter = () => {
+	degreesPerHour = degreesPerHour + rotationPerHour;
+	hour_hand.style.tranform = `rotate(${degreesPerHour}deg)`
+}
+
+const startClock = () => {
+	setInterval(secondsCounter, 1000)
+	setInterval(minutesCounter, 1000)
+	setInterval(hoursCounter, 1000)
+}
+
+//startClock()
